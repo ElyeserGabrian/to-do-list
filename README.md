7c2ac9daec8281220e8426a28997557369b4b6e2
elyeser <elyoliveiranunes@gmail.com>
2024-04-07 21:25:42 -0300

    add to do list

---
+++ README.md
@@ -0,0 +1,13 @@
+<h1 align="center">Projeto To Do List</h1>
+<p>
+Neste projeto eu fiz uma To Do List, com o objetivo de adquirir conhecimento na linguagem de Programação JavaScript
+</p>
+<p align="center">
+📁 Projeto
+<a href="">Saiba mais...</a>
+</p>
+
+## 🚀Tecnologias
+- VsCode 💻
+- JavaScript 👨🏽‍💻
+- Git & GitHub 👨🏽‍💻
--- script.js
+++ script.js
@@ -14,3 +14,46 @@ if (nomeArmazenado) {
     // Redirecionar para a página de login se o nome não estiver disponível
     window.location.href = "login.html";
 }
+
+//Adicionar task ao clicar no botao
+
+ function adicionaTask(){
+    let novatask = document.getElementById('input-new-task');
+    let task = novatask.value
+    
+
+    let ul = document.getElementById('to-do-list')
+    //criacao da div para a li e os botoes
+    let div = document.createElement('div')
+    // colocando uma classe para a div
+    div.classList.add("li-task")
+    // criando os botoes
+    let btn_excluir = document.createElement('button')
+    let lixo = document.createElement('i')
+    lixo.className = "fas fa-trash"
+    // classe para os botoes
+    btn_excluir.classList.add("btn_excluir")
+    // criando a li
+    let lista = document.createElement('li');
+    // colocando o texto do input dentro da li
+    lista.textContent = task
+
+    // aqui colocando no html para ver na pagina
+    ul.appendChild(div);    
+    div.appendChild(lista)
+    div.appendChild(btn_excluir)
+    btn_excluir.appendChild(lixo)
+
+    btn_excluir.onclick = function(){
+        let resposta = confirm("Você deseja excluir?")
+        if (resposta == true){
+            ul.removeChild(div)
+            console.log("Usuario clicou Ok")
+
+        }
+        else{
+            console.log("usuario clicou em cancelar")
+        }
+    }
+
+ }
